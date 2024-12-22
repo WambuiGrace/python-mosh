7f19ea07ee96099057523906d122c4e41b96c8bc
Grace Wambui <wambuigm51@yahoo.com>
2024-12-22 21:51:43 +0300

    Add currency converter

--- 05_currency_converter.py
+++ 05_currency_converter.py
@@ -12,3 +12,22 @@ while True:
         break
     except ValueError:
         print('Invalid amount!')
+
+
+currencies = ('USD', 'EUR', 'GBP', 'INR', 'AUD', 'CAD', 'SGD', 'CHF', 'MYR', 'JPY', 'CNY')
+while True:
+    source_currency = input('Enter the source currency: ')
+    if source_currency not in currencies:
+        print('Invalid source currency!')
+    else:
+        break
+
+while True:
+    target_currency = input('Enter the target currency: ')
+    if target_currency not in currencies:
+        print('Invalid target currency!')
+    else:
+        break
+
+
+print(f'{amount} {source_currency} is equal to {amount} {target_currency}')
