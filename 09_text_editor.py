66efbc4e5d0ae4bc299df44878996393045de6d0
Grace Wambui <wambuigm51@yahoo.com>
2025-01-31 10:43:28 +0300

    Refactor text editor functions for improved file handling and user input

--- 09_text_editor.py
+++ 09_text_editor.py
@@ -1,35 +1,39 @@
 import os
 
+def read_file(filename):
+    with open(filename, 'r') as file:
+        return file.read()
+       
+def write_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+
+def get_user_iput():
+    print('\n Enter the content of the file. Type "SAVE" on a new line to save and exit.')
+
+    lines = []
+    while True:
+        line = input()
+        if line == 'SAVE':
+            break
+        lines.append(line)
+    return '\n'.join(lines)
+
 def main():
     # Get the input from the user
     filename = input('Enter the filename to open or create: ').strip()
     try:
         if os.path.exists(filename):
-            with open(filename, 'r') as file:
-                content = file.read()
-                print(content)
+            print(read_file(filename))
         else:
-            with open(filename, 'w') as file:
-                print('File created successfully.')
-                pass
+           write_file(filename, '')
+           print('File created successfully.')
+            
+        content = get_user_iput()
+        write_file(filename, content) 
+        print(f'{filename} saved successfully.')
     except OSError:
-        print('Error opening the file. Please try again.')
-        return
-    
-    print('\n Enter the content of the file. Type "SAVE" on a new line to save and exit.')
-
-    content = []
-    while True:
-        line = input()
-        if line == 'SAVE':
-            break
-        content.append(line)
-    try:
-        with open(filename, 'w') as file:
-            file.write('\n'.join(content)) 
-            print(f'{filename} saved successfully.')
-    except OSError:
-        print(f'Error saving the {filename} file.')
+        print(f'Error opening the {filename} file.')
         return
 
 if __name__ == "__main__":
